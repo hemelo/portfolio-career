01ab1834fa23575aba9b857391159bec343284c1
Henrique Melo <hmelo2509@gmail.com>
2023-02-22 10:15:57 -0300

    add: tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#2563eb',
+        secondary: '#bef264'
+      }
+
+    },
+  },
+  plugins: [],
+}
